e33039b0acc4b6f991b1c09b7cd1f1c0ceec6410
NAHID12105080 <nahid151341@gmail.com>
2024-10-19 11:26:39 +0600

    mongodb setup

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 import dotenv from "dotenv";
 import { ApolloServer } from "@apollo/server"; // preserve-line
 import { startStandaloneServer } from "@apollo/server/standalone"; // preserve-line
+import { connectDB } from "./database/database.js";
+import { schema } from "./graphql/schema.js";
+
 dotenv.config({ path: "./.env" });
 
 export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
 const port = Number(process.env.PORT) || 3000;
+const mongoURI = process.env.MONGO_URI!;
+connectDB(mongoURI);
 
 const server = new ApolloServer({
-  typeDefs: `
-    type Query {
-      hello: String
-    }
-  `,
+  typeDefs: schema,
   resolvers: {
     Query: {
       hello: () => "Hello, world!",
